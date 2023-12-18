d112f85dba35a08a1c8e5ddb17bcfe821e01ed63
ilaydadaban00 <145451273+ilaydadaban00@users.noreply.github.com>
2023-12-18 10:55:50 +0300

    Add files via upload

---
+++ math sınıfı/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace math_sınıfı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.lblKArehesaplama = new System.Windows.Forms.Label();
+            this.lblKok = new System.Windows.Forms.Label();
+            this.lblUst = new System.Windows.Forms.Label();
+            this.lblYuvarlama = new System.Windows.Forms.Label();
+            this.lblAsagıYUvarlama = new System.Windows.Forms.Label();
+            this.lblYukarıYUvarlama = new System.Windows.Forms.Label();
+            this.lblMutlakdeger = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(85, 73);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(75, 23);
+            this.btnHesapla.TabIndex = 0;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Location = new System.Drawing.Point(75, 27);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi.TabIndex = 1;
+            // 
+            // lblKArehesaplama
+            // 
+            this.lblKArehesaplama.AutoSize = true;
+            this.lblKArehesaplama.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKArehesaplama.Location = new System.Drawing.Point(343, 33);
+            this.lblKArehesaplama.Name = "lblKArehesaplama";
+            this.lblKArehesaplama.Size = new System.Drawing.Size(94, 13);
+            this.lblKArehesaplama.TabIndex = 2;
+            this.lblKArehesaplama.Text = "KArehesaplama";
+            // 
+            // lblKok
+            // 
+            this.lblKok.AutoSize = true;
+            this.lblKok.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKok.Location = new System.Drawing.Point(343, 83);
+            this.lblKok.Name = "lblKok";
+            this.lblKok.Size = new System.Drawing.Size(29, 13);
+            this.lblKok.TabIndex = 3;
+            this.lblKok.Text = "Kok";
+            // 
+            // lblUst
+            // 
+            this.lblUst.AutoSize = true;
+            this.lblUst.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblUst.Location = new System.Drawing.Point(343, 135);
+            this.lblUst.Name = "lblUst";
+            this.lblUst.Size = new System.Drawing.Size(26, 13);
+            this.lblUst.TabIndex = 4;
+            this.lblUst.Text = "Ust";
+            // 
+            // lblYuvarlama
+            // 
+            this.lblYuvarlama.AutoSize = true;
+            this.lblYuvarlama.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYuvarlama.Location = new System.Drawing.Point(343, 195);
+            this.lblYuvarlama.Name = "lblYuvarlama";
+            this.lblYuvarlama.Size = new System.Drawing.Size(66, 13);
+            this.lblYuvarlama.TabIndex = 5;
+            this.lblYuvarlama.Text = "Yuvarlama";
+            this.lblYuvarlama.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lblAsagıYUvarlama
+            // 
+            this.lblAsagıYUvarlama.AutoSize = true;
+            this.lblAsagıYUvarlama.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAsagıYUvarlama.Location = new System.Drawing.Point(343, 238);
+            this.lblAsagıYUvarlama.Name = "lblAsagıYUvarlama";
+            this.lblAsagıYUvarlama.Size = new System.Drawing.Size(99, 13);
+            this.lblAsagıYUvarlama.TabIndex = 6;
+            this.lblAsagıYUvarlama.Text = "AsagıYUvarlama";
+            // 
+            // lblYukarıYUvarlama
+            // 
+            this.lblYukarıYUvarlama.AutoSize = true;
+            this.lblYukarıYUvarlama.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYukarıYUvarlama.Location = new System.Drawing.Point(343, 285);
+            this.lblYukarıYUvarlama.Name = "lblYukarıYUvarlama";
+            this.lblYukarıYUvarlama.Size = new System.Drawing.Size(104, 13);
+            this.lblYukarıYUvarlama.TabIndex = 7;
+            this.lblYukarıYUvarlama.Text = "YukarıYUvarlama";
+            // 
+            // lblMutlakdeger
+            // 
+            this.lblMutlakdeger.AutoSize = true;
+            this.lblMutlakdeger.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMutlakdeger.Location = new System.Drawing.Point(343, 329);
+            this.lblMutlakdeger.Name = "lblMutlakdeger";
+            this.lblMutlakdeger.Size = new System.Drawing.Size(77, 13);
+            this.lblMutlakdeger.TabIndex = 8;
+            this.lblMutlakdeger.Text = "Mutlakdeger";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMutlakdeger);
+            this.Controls.Add(this.lblYukarıYUvarlama);
+            this.Controls.Add(this.lblAsagıYUvarlama);
+            this.Controls.Add(this.lblYuvarlama);
+            this.Controls.Add(this.lblUst);
+            this.Controls.Add(this.lblKok);
+            this.Controls.Add(this.lblKArehesaplama);
+            this.Controls.Add(this.txtSayi);
+            this.Controls.Add(this.btnHesapla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.TextBox txtSayi;
+        private System.Windows.Forms.Label lblKArehesaplama;
+        private System.Windows.Forms.Label lblKok;
+        private System.Windows.Forms.Label lblUst;
+        private System.Windows.Forms.Label lblYuvarlama;
+        private System.Windows.Forms.Label lblAsagıYUvarlama;
+        private System.Windows.Forms.Label lblYukarıYUvarlama;
+        private System.Windows.Forms.Label lblMutlakdeger;
+    }
+}
+
---
+++ math sınıfı/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace math_sınıfı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            double sayi = Convert.ToSingle(txtSayi.Text);
+
+
+
+
+            double mutlak=Math.Abs(sayi);
+            double yuvarlamaUSt=Math.Ceiling(sayi);
+            double kok=Math.Sqrt(sayi);
+            double us = Math.Pow(sayi, 2);
+            double asıgıYuvarlama=Math.Floor(sayi);
+            double yuvarlama=Math.Round(sayi);
+
+
+
+
+            lblMutlakdeger.Text = "  sayının mutlak degeri:     " + mutlak.ToString();
+            lblYukarıYUvarlama.Text = "    sayının uste yuvarlama:  " + yuvarlamaUSt.ToString();
+            lblKok.Text="    sayının kökunu alama    "+kok.ToString();
+            lblUst.Text = " sayının ustu     " + us.ToString();
+            lblAsagıYUvarlama.Text = "    sayının asagı yuvarlanması    " + asıgıYuvarlama.ToString();
+            lblYuvarlama.Text = "    yuvarlama    " + yuvarlama.ToString();
+         
+
+        }
+    }
+}
